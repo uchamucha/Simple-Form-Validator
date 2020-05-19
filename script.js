13e4dfb540096cde376a6017307a9ae612aa3926
Usamath PC <usamathpallikkachalil@gmail.com>
2020-05-20 00:51:47 +0530

    refactor: added functions to remove redundancy

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//select elements to be manipulated
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
@@ -18,11 +19,55 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-//check email validity
-function isValidEmail(email) {
+//check email validity (refactored into one function)
+function checkEmail(input) {
   //use regular expression (taken from stackoverflow)
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+
+  if (re.test(String(input.value).toLowerCase())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Invalid Email");
+  }
+}
+
+//extra functions for refactoring
+
+//check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+//Capitalize first word of error fields
+function getFieldName(x) {
+  return x.id.charAt(0).toUpperCase() + x.id.slice(1);
+}
+
+//check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at less than ${max} characters`
+    );
+  }
+}
+
+//check if passwords match
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
 }
 
 //Event listeners
@@ -30,27 +75,11 @@ form.addEventListener("submit", function (e) {
   // take over the form before it finishes submitting to the file
   e.preventDefault();
 
-  //error and success conditions anc function calls
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-  if (email.value === "") {
-    showError(email, "email is required");
-  } else if (!isValidEmail(email.value)) {
-    showError(email, "email is not valid");
-  } else {
-    showSuccess(email);
-  }
-  if (password.value === "") {
-    showError(password, "password is required");
-  } else {
-    showSuccess(password);
-  }
-  if (password2.value === "") {
-    showError(password2, "password2 is required");
-  } else {
-    showSuccess(password2);
-  }
+  //refactored using functions---->
+  checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  //   checkLength(password2, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
